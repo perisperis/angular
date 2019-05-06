3ce5779a7b62453d6081b95a97926dcd3ce35c99
a508571 <a508571@MC0XHTMC.ww930.my-it-solutions.net>
2019-05-06 17:31:04 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AlertModule} from 'ngx-bootstrap';
+import { ButtonsModule} from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap'; 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ProgressbarModule} from 'ngx-bootstrap'; 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+            ButtonsModule.forRoot(),
+            AlertModule.forRoot(),
+            ProgressbarModule.forRoot(),
+            TabsModule.forRoot(),
+            CarouselModule.forRoot(),
+            
     BrowserModule,
     AppRoutingModule
   ],
