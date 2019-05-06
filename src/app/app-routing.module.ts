058bee4f0d5083caf909c7acf5eab2021033ca98
a508571 <a508571@MC0XHTMC.ww930.my-it-solutions.net>
2019-05-06 18:31:39 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { InstagramComponent } from './instagram/instagram.component';
+//import { GoogleDriveComponent } from './google-drive/google-drive.component';
+//import { TwitterComponent } from './twitter/twitter.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { HttpClientModule } from '@angular/common/http';
+//import { FormsModule } from '@angular/forms';
+//import { ReactiveFormsModule } from '@angular/forms';
+//import { AlertModule} from 'ngx-bootstrap';
+//import { ButtonsModule} from 'ngx-bootstrap';
+//import { TabsModule } from 'ngx-bootstrap'; 
+//import { CarouselModule } from 'ngx-bootstrap/carousel';
+//import { ProgressbarModule} from 'ngx-bootstrap'; 
+//import { AppRoutingModule } from './app-routing.module';
+//import { AppComponent } from './app.component';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { AlertModule} from 'ngx-bootstrap';
 import { ButtonsModule} from 'ngx-bootstrap';
 import { TabsModule } from 'ngx-bootstrap'; 
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ProgressbarModule} from 'ngx-bootstrap'; 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//import { InstagramComponent } from './instagram/instagram.component';
+//import { GoogleDriveComponent } from './google-drive/google-drive.component';
+//import { TwitterComponent } from './twitter/twitter.component';
+
+import { CommonModule } from '@angular/common';
 
 
 import { InstagramComponent } from './instagram/instagram.component';
 //import { GoogleDriveComponent } from './google-drive/google-drive.component';
 import { TwitterComponent } from './twitter/twitter.component';
+
 
 @NgModule({
   declarations: [
@@ -26,13 +44,17 @@ import { TwitterComponent } from './twitter/twitter.component';
     TwitterComponent
   ],
   imports: [
+            
             ButtonsModule.forRoot(),
             AlertModule.forRoot(),
             ProgressbarModule.forRoot(),
             TabsModule.forRoot(),
             CarouselModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
